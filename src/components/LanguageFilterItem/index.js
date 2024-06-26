a20c7bbc37ebd8c7bb948d4823f84fabeb4c2953
karnataka saikumar <karnatakasaikumar1234@gmail.com>
2024-06-26 08:50:58 +0000

    Github-Popular-Repos

---
+++ src/components/LanguageFilterItem/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {eachLanguage, isActive, changeActiveBtn} = props
+  const {id, language} = eachLanguage
+  const btnHighLight = isActive ? 'high-light' : ''
+
+  const onClickingLanguageBtn = () => {
+    changeActiveBtn(id)
+  }
+
+  return (
+    <li>
+      <button
+        type="button"
+        className={`each-language ${btnHighLight}`}
+        onClick={onClickingLanguageBtn}
+      >
+        {language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
